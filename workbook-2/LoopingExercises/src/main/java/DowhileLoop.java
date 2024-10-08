a147b73efc7426f63a899e0c91f8b347a4d9c174
Mordecai_Anagaw <mordecaianagaw@gmail.com>
2024-10-08 15:46:47 -0700

    Looping Exercise

---
+++ workbook-2/LoopingExercises/src/main/java/DowhileLoop.java
@@ -0,0 +1,11 @@
+public class DowhileLoop {
+        public static void main(String[] args) {
+            int count = 0;
+
+            do {
+                System.out.println("I love Java");
+                count++;
+            } while (count < 5);
+        }
+    }
+
